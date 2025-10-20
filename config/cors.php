7b17630b6ca4a00bb8b5fafe6cbb7fcaaa8c5930
Mohammed Nasr <mnasr48@gmail.com>
2025-10-20 15:40:19 +0300

    Add Vercel domain to CORS allowed origins

--- config/cors.php
+++ config/cors.php
@@ -5,7 +5,10 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000'],
+    'allowed_origins' => [
+        'http://localhost:3000',
+        'https://mora-mu.vercel.app',
+    ],
 
     'allowed_origins_patterns' => [],
 
